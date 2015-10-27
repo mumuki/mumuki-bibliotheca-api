1c3e9a2c7b78ee5b0a4d541f948b13d71efa31de
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-10-27 15:51:25 -0300

    Pull up

--- lib/git_io/guide.rb
+++ lib/git_io/guide.rb
@@ -3,27 +3,33 @@ module GitIo
 
   end
 
-  class Guide
+  class JsonWrapper
     def initialize(json)
-      @json = {beta: false,
-               learning: false,
-               original_id_format: '%05d'}.merge(json)
-    end
-
-    def exercises
-      @exercises ||= @json[:exercises].map { |e| OpenStruct.new e }
+      @json = defaults.merge(json)
     end
 
     def as_json(options)
       @json.as_json(options)
     end
 
-    def language
-      @language ||= Language.find_by_name(@json[:language])
-    end
-
     def method_missing(name, *args)
       @json[name]
+    end
+  end
+
+  class Guide < JsonWrapper
+    def defaults
+      {beta: false,
+       learning: false,
+       original_id_format: '%05d'}
+    end
+
+    def exercises
+      @exercises ||= @json[:exercises].map { |e| OpenStruct.new e }
+    end
+
+    def language
+      @language ||= Language.find_by_name(@json[:language])
     end
 
     def format_original_id(exercise)
