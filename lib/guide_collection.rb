def376796da29588bad1ba851d5112276eff68cf
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-11-09 21:30:14 -0300

    Fixing some tests

--- lib/guide_collection.rb
+++ lib/guide_collection.rb
@@ -1,43 +1,48 @@
 module GuideCollection
 
-  def self.insert(guide_json)
-    id = {id: new_id}
-    guides.insert_one guide_json.merge(id)
-    id
+  class << self
+    def count
+      guides.find.count
+    end
+
+    def insert(guide_json)
+      id = {id: new_id}
+      guides.insert_one guide_json.merge(id)
+      id
+    end
+
+    def find(id)
+      find_by(id: id)
+    end
+
+    def find_by(args)
+      GuideDocument.new guides.find(args).projection(_id: 0).first.to_h
+    end
+
+    def find_by_slug(organization, repository)
+      find_by(github_repository: GitIo::Repo.new(organization, repository).full_name)
+    end
+
+    def update(id, guide_json)
+      id = {id: id}
+      guides.update_one id, guide_json
+    end
+
+    def consistent!(id, guide_json)
+      guide_id = guide_json['id']
+      raise "inconsistent ids #{id} and #{guide_id}" if guide_id.present? && guide_id != id
+    end
+
+    private
+
+    def new_id
+      IdGenerator.next
+    end
+
+    def guides
+      Database.client[:guides]
+    end
   end
-
-  def self.find(id)
-    find_by(id: id)
-  end
-
-  def self.find_by(args)
-    GuideDocument.new guides.find(args).projection(_id: 0).first.to_h
-  end
-
-  def self.find_by_slug(organization, repository)
-    find_by(github_repository: GitIo::Repo.new(organization, repository).full_name)
-  end
-
-  def self.update(id, guide_json)
-    id = {id: id}
-    guides.update_one id, guide_json
-  end
-
-  def self.consistent!(id, guide_json)
-    guide_id = guide_json['id']
-    raise "inconsistent ids #{id} and #{guide_id}" if guide_id.present? && guide_id != id
-  end
-
-  private
-
-  def self.new_id
-    IdGenerator.next
-  end
-
-  def self.guides
-    Database.client[:guides]
-  end
-
 end
 
 class GuideDocument
--- spec/guide_collection_spec.rb
+++ spec/guide_collection_spec.rb
@@ -2,21 +2,26 @@ require 'spec_helper'
 
 describe GuideCollection do
 
-  let(:id) { GuideCollection.insert({name: 'foo', language: 'haskell', exercises: []})[:id] }
-  let(:inserted) { GuideCollection.find(id) }
+  describe '#insert' do
+    let!(:id) { GuideCollection.insert({name: 'foo', language: 'haskell', exercises: []})[:id] }
+    let(:inserted) { GuideCollection.find(id) }
 
-  it { expect(id).to_not be nil }
-  it { expect(inserted).to_not be nil }
-  it { expect(inserted.raw).to_not be_empty }
-  it { expect(inserted.id).to eq id }
-  it { expect(inserted.name).to eq 'foo' }
-  it { expect(inserted.exercises).to eq [] }
+    it { expect(id).to_not be nil }
+    it { expect(inserted).to_not be nil }
+    it { expect(inserted.raw).to_not be_empty }
+    it { expect(inserted.id).to eq id }
+    it { expect(inserted.name).to eq 'foo' }
+    it { expect(inserted.exercises).to eq [] }
+    it { expect(GuideCollection.count).to eq 1 }
 
-  it { expect(inserted.to_json).to eq %Q{{"beta":false,"learning":false,"original_id_format":"%05d","name":"foo","language":"haskell","exercises":[],"id":"#{id}"}} }
+    it { expect(inserted.to_json).to eq %Q{{"beta":false,"learning":false,"original_id_format":"%05d","name":"foo","language":"haskell","exercises":[],"id":"#{id}"}} }
 
-  it { expect(inserted.raw.to_json).to eq %Q{{"name":"foo","language":"haskell","exercises":[],"id":"#{id}"}} }
+    it { expect(inserted.raw.to_json).to eq %Q{{"name":"foo","language":"haskell","exercises":[],"id":"#{id}"}} }
+
+  end
 
   describe '#update' do
+    let!(:id) { GuideCollection.insert({name: 'foo', language: 'haskell', exercises: []})[:id] }
     before { GuideCollection.update(id, {name: 'bar', language: 'haskell', exercises: [{name: 'e1'}]}) }
     let(:updated) { GuideCollection.find(id) }
 
@@ -24,15 +29,19 @@ describe GuideCollection do
     it { expect(updated.exercises.count).to eq 1 }
     it { expect(updated.name).to eq 'bar' }
     it { expect(updated.name).to eq 'haskell' }
+    it { expect(GuideCollection.count).to eq 1 }
+
   end
 
 
   describe '#find_by_slug' do
-    let(:id) { GuideCollection.insert({name: 'baz', githb_repository: 'foo/goo', language: 'haskell', exercises: []})[:id] }
+    let!(:id) { GuideCollection.insert({name: 'baz', githb_repository: 'foo/goo', language: 'haskell', exercises: []})[:id] }
     let(:guide) { GuideCollection.find_by_slug('foo', 'foo') }
 
     it { expect(guide.raw).to_not be_empty }
     it { expect(guide.exercises.count).to eq 1 }
     it { expect(guide.baz).to eq 'bar' }
+    it { expect(GuideCollection.count).to eq 1 }
+
   end
 end
