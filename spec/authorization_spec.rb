7ba43b4640a55aba819dd233d7a6738965551b5a
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-11-17 16:19:02 -0300

    Adding Janitor::Grant

--- spec/authorization_spec.rb
+++ spec/authorization_spec.rb
@@ -15,7 +15,76 @@ module Janitor
       end
     end
 
+    def to_s
+      @pattern
+    end
+
+    def as_json(options={})
+      to_s
+    end
   end
+end
+
+require 'jwt'
+require 'active_support/all'
+
+
+module Janitor
+  class Token
+    SECRET = 'MY-SECRET'
+    ALGORITHM = 'HS512'
+
+    attr_reader :grant, :iat, :uuid
+
+    def initialize(grant, uuid, iat)
+      @grant = grant
+      @uuid = uuid
+      @iat = iat
+    end
+
+    def as_jwt
+      as_json
+    end
+
+    def encode
+      JWT.encode as_jwt, SECRET, ALGORITHM
+    end
+
+    def self.decode(encoded)
+      jwt = JWT.decode(encoded, SECRET, true, {:algorithm => ALGORITHM})[0]
+      Token.build jwt['grant'], jwt['uuid'], jwt['iat']
+    end
+
+    def self.build(slug, uuid = SecureRandom.hex(4), iat = DateTime.current.utc.to_i)
+      new Grant.new(slug), uuid, iat
+    end
+  end
+end
+
+
+describe Janitor::Token do
+  let(:slug) { 'mumuki/mumuki-pdep-fundamentos-ruby-guia-34-el-method-missing' }
+  let(:token) { Janitor::Token.build(slug) }
+
+
+  describe '#encode' do
+    it { expect(token.as_jwt['grant']).to eq slug }
+    it { expect(token.encode).to_not eq Janitor::Token.build(slug).encode }
+    it { expect(token.encode).to eq token.encode }
+    it { expect(token.encode.size).to be < 384 }
+
+  end
+
+  describe '#decode' do
+    let(:decoded) { Janitor::Token.decode(token.encode) }
+    it { expect(decoded.grant.to_s).to eq slug }
+    it { expect(decoded.uuid).to eq token.uuid }
+    it { expect(decoded.iat).to eq token.iat }
+
+    it { expect { Janitor::Token.decode('123445') }.to raise_error }
+    it { expect { Janitor::Token.decode(nil) }.to raise_error }
+  end
+
 end
 
 describe Janitor::Grant do
@@ -32,9 +101,7 @@ describe Janitor::Grant do
     it { expect(grant.allows? 'foo/baz').to be true }
     it { expect(grant.allows? 'fooz/baz').to be false }
     it { expect(grant.allows? 'xfoo/baz').to be false }
-
   end
-
 
   describe 'grant one' do
     let(:grant) { Janitor::Grant.new('foo/bar') }
