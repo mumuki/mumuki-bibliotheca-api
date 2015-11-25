5e4ab3dec4ced505997d64509422f39ad3bebe21
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-11-25 13:57:20 -0300

    Adding new language fields

--- lib/git_io/language.rb
+++ lib/git_io/language.rb
@@ -1,16 +1,22 @@
 module GitIo
   class Language
-    attr_accessor :name, :extension, :test_extension
+    attr_accessor :name, :extension, :test_extension, :ace_mode, :devicon
 
     def initialize(args={})
       @name = args[:name]
       @extension = args[:extension]
       @test_extension = args[:test_extension] || @extension
+      @ace_mode = args[:ace_mode]
+      @devicon = args[:devicon]
     end
 
     def as_json(options={})
       if options[:full_language]
-        {name: name, extension: extension}
+        {name: name,
+         extension: extension,
+         test_extension: test_extension,
+         ace_mode: ace_mode,
+         devicon: devicon}
       else
         name
       end
@@ -22,14 +28,35 @@ module GitIo
     end
 
     LANGUAGES = [
-        Language.new(name: 'haskell', extension: 'hs'),
-        Language.new(name: 'java', extension: 'java'),
-        Language.new(name: 'wollok', extension: 'wlk'),
-        Language.new(name: 'c', extension: 'c'),
-        Language.new(name: 'prolog', extension: 'pl'),
-        Language.new(name: 'ruby', extension: 'rb'),
-        Language.new(name: 'gobstones', extension: 'gbs', test_extension: 'yml'),
-        Language.new(name: 'javascript', extension: 'js')]
-
+        Language.new(name: 'haskell',
+                     extension: 'hs',
+                     ace_mode: 'haskell',
+                     devicon: 'haskell'),
+        Language.new(name: 'java',
+                     extension: 'java',
+                     ace_mode: 'java',
+                     devicon: 'java'),
+        Language.new(name: 'wollok',
+                     extension: 'wlk',
+                     ace_mode: 'wollok'),
+        Language.new(name: 'c',
+                     extension: 'c',
+                     ace_mode: 'c'),
+        Language.new(name: 'prolog',
+                     extension: 'pl',
+                     ace_mode: 'prolog',
+                     devicon: 'prolog'),
+        Language.new(name: 'ruby',
+                     extension: 'rb',
+                     ace_mode: 'ruby',
+                     ruby_rough: 'ruby_rough'),
+        Language.new(name: 'gobstones',
+                     extension: 'gbs',
+                     test_extension: 'yml',
+                     ace_mode: 'gobstones'),
+        Language.new(name: 'javascript',
+                     extension: 'js',
+                     ace_mode: 'javascript',
+                     devicon: 'javascript_badge')]
   end
 end
