2728ff83c6dfe312603a053b829805de553bfa7d
aguspina <aguspina87@gmail.com>
2015-12-07 01:41:50 -0300

    Fix export guide by removing logs

--- lib/bibliotheca/io/export.rb
+++ lib/bibliotheca/io/export.rb
@@ -9,23 +9,18 @@ module Bibliotheca::IO
     end
 
     def repo
-      @repo ||= Bibliotheca::Repo.from_full_name(guide[:slug])
+      @repo ||= Bibliotheca::Repo.from_full_name(guide.slug)
     end
 
     def run!
-      Rails.logger.info "Exporting guide #{guide[:name]}"
+      bot.ensure_exists! repo
+      with_local_repo do |dir, local_repo|
 
-      log = ExportLog.new
-      log.with_error_logging do
-        bot.ensure_exists! repo
-        with_local_repo do |dir, local_repo|
+        GuideWriter.new(dir, log).write_guide! guide
 
-          GuideWriter.new(dir, log).write_guide! guide
-
-          local_repo.add(all: true)
-          local_repo.commit("Mumuki Export on #{Time.now}")
-          local_repo.push
-        end
+        local_repo.add(all: true)
+        local_repo.commit("Mumuki Export on #{Time.now}")
+        local_repo.push
       end
     end
   end
