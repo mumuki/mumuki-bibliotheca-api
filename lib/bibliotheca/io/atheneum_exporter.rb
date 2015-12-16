054edbbc2f8afe0d3c3bcaf882ad3bf56afffc37
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-12-16 15:22:51 -0300

    Moving env variables to from_env

--- lib/bibliotheca/io/atheneum_exporter.rb
+++ lib/bibliotheca/io/atheneum_exporter.rb
@@ -1,17 +1,19 @@
 module Bibliotheca::IO
   class AtheneumExporter
-    attr_accessor :url
+    attr_accessor :url, :user, :password
 
-    def initialize(url)
+    def initialize(url, user, password)
       @url = url
+      @user = user
+      @password = password
     end
 
     def self.new_from_env
-      new(ENV['ATHENEUM_URL'])
+      new ENV['ATHENEUM_URL'], ENV['ATHENEUM_USER'], ENV['ATHENEUM_PASSWORD']
     end
 
     def run!(guide)
-      RestClient::Request.execute(method: :post, url: @url, user: ENV['ATHENEUM_USER'], password: ENV['ATHENEUM_PASSWORD'], payload: guide)
+      RestClient::Request.execute(method: :post, url: url, user: user, password: password, payload: guide)
     end
   end
 end
