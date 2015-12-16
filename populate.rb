8a8236b27758e9e3ffc0eb14da7dc7795d265015
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-12-16 14:59:37 -0300

    Now populate scripts load all guides from GH

---
+++ populate.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+#require 'bundler/setup'
+
+gem 'octokit'
+gem 'rest-client'
+
+require 'rest-client'
+require 'octokit'
+
+
+['pdep-utn', 'sagrado-corazon-alcal', 'mumuki', 'arquitecturas-concurrentes'].flat_map do |org|
+  Octokit.repos(org).map(&:full_name).select { |it| it =~ /guia/ }
+end.each do |slug|
+  puts slug 
+  begin
+    RestClient.post "http://bibliotheca.mumuki.io/guides/import/#{slug}", {}
+  rescue => e
+    puts e
+  end
+end
+
