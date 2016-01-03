0862eecffd9b068b826837a84c91fdb8f3ea81d2
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-01-03 11:09:08 -0300

    Making json wrapper lazy

--- lib/bibliotheca/json_wrapper.rb
+++ lib/bibliotheca/json_wrapper.rb
@@ -1,22 +1,27 @@
 module Bibliotheca
   class JsonWrapper
+    attr_accessor :raw
+
     def initialize(json)
-      json = json.to_h.symbolize_keys
-      @json = defaults.
-          merge(json).
-          merge(transforms(json))
+      @raw = json.to_h.symbolize_keys
     end
 
     def as_json(options={})
-      @json.as_json(options)
+      json.as_json(options)
     end
 
     def method_missing(name, *args)
-      @json[name]
+      json[name]
     end
 
     def transforms(original)
       {}
+    end
+
+    def json
+      @json ||= defaults.
+                merge(@raw).
+                merge(transforms(@raw))
     end
   end
 end
