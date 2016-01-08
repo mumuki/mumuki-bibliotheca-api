fb97adaae0bca7b02eb0df599b6eac06cf28b8cd
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-01-08 19:52:03 -0300

    Not failing if there are no files

--- lib/bibliotheca/io/export.rb
+++ lib/bibliotheca/io/export.rb
@@ -17,13 +17,21 @@ module Bibliotheca::IO
     end
 
     def run_in_local_repo(dir, local_repo)
-      local_repo.remove('*')
+      clear_repo local_repo
       GuideWriter.new(dir, log).write_guide! guide
       local_repo.add(all: true)
       local_repo.commit("Mumuki Export on #{Time.now}")
       local_repo.push
     rescue Git::GitExecuteError => e
       puts "Could not export guide #{guide.slug} to git #{e}"
+    end
+
+    private
+
+    def clear_repo(local_repo)
+      local_repo.remove('*')
+    rescue Git::GitExecuteError => e
+      puts 'Nothing to clean, repo seems to be empty'
     end
   end
 
