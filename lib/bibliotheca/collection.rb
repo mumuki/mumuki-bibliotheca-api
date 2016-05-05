f1de3ceb87ba4e66933088959165db31c3f769b4
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-05 18:39:09 -0300

    Merging arrays collection files

--- lib/bibliotheca/collection.rb
+++ lib/bibliotheca/collection.rb
@@ -4,9 +4,6 @@ end
 
 require_relative './collection/exercise_not_found_error'
 require_relative './collection/with_slug'
-require_relative './collection/guide_array'
-require_relative './collection/language_array'
 require_relative './collection/languages'
 require_relative './collection/guides'
 require_relative './collection/books'
-require_relative './collection/book_array'
--- lib/bibliotheca/collection/books.rb
+++ lib/bibliotheca/collection/books.rb
@@ -1,23 +1,34 @@
-module Bibliotheca::Collection::Books
-  extend Mumukit::Service::Collection
-  extend Bibliotheca::Collection::WithSlug
+module Bibliotheca::Collection
+  module Books
+    extend Mumukit::Service::Collection
+    extend Bibliotheca::Collection::WithSlug
 
-  private
+    private
 
-  def self.mongo_collection_name
-    :books
+    def self.mongo_collection_name
+      :books
+    end
+
+    def self.mongo_database
+      Bibliotheca::Database
+    end
+
+    def self.wrap(it)
+      Bibliotheca::Book.new(it)
+    end
+
+    def self.wrap_array(it)
+      Bibliotheca::Collection::BookArray.new(it)
+    end
   end
 
-  def self.mongo_database
-    Bibliotheca::Database
-  end
+  class BookArray < Mumukit::Service::JsonArrayWrapper
+    def options
+      {only: [:id, :name, :slug]}
+    end
 
-  def self.wrap(it)
-    Bibliotheca::Book.new(it)
-  end
-
-  def self.wrap_array(it)
-    Bibliotheca::Collection::BookArray.new(it)
+    def key
+      :books
+    end
   end
 end
-
