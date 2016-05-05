262cbbeecdefcfd57bc53da4c6648af2e75e3d50
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-05 16:42:36 -0300

    Extracting languages specs

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -35,23 +35,6 @@ describe 'routes' do
     Sinatra::Application
   end
 
-  describe('get /languages') do
-    before do
-      get '/languages'
-    end
-
-    it { expect(last_response).to be_ok }
-    skip { expect(last_response.body).to json_eq({languages: [
-        {name: 'haskell', extension: 'hs'},
-        {name: 'java', extension: 'java'},
-        {name: 'wollok', extension: 'wlk'},
-        {name: 'c', extension: 'c'},
-        {name: 'prolog', extension: 'pl'},
-        {name: 'ruby', extension: 'rb'},
-        {name: 'gobstones', extension: 'gbs'},
-        {name: 'javascript', extension: 'js'}]}) }
-  end
-
   describe('get /books') do
     before do
       get '/books'
