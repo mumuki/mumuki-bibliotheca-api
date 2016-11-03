b32d9697b59219bb59330aded114b748807e2d2d
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-11-03 00:28:50 -0300

    Adding upload editor

--- lib/bibliotheca/exercise.rb
+++ lib/bibliotheca/exercise.rb
@@ -11,7 +11,7 @@ module Bibliotheca
     def errors
       [
         ("Invalid layout #{layout}" unless [nil, 'input_right', 'input_bottom'].include? layout),
-        ("Invalid editor #{editor}" unless [nil, 'code', 'multiple_choice', 'single_choice', 'hidden', 'text'].include? editor),
+        ("Invalid editor #{editor}" unless [nil, 'code', 'multiple_choice', 'single_choice', 'hidden', 'text', 'upload'].include? editor),
         ('Name must be present' unless name.present?),
         ('Name must not contain a / character' if name.include? '/'),
         ("Invalid exercise type #{type}" unless [nil, 'problem', 'playground'].include? type),
