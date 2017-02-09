3ead1f708db995a4a008d1cb0b44ced53c6c3377
Agustin Pina <aguspina87@gmail.com>
2017-02-09 17:01:59 -0300

    Sett httponly cookie

--- app/session_store.rb
+++ app/session_store.rb
@@ -1,5 +1,9 @@
 require 'sinatra/cookies'
 
 use Rack::Session::Cookie,
+<<<<<<< Updated upstream
     key: '_mumuki_bibliotheca_session',
     domain: ENV['MUMUKI_COOKIES_DOMAIN']
+=======
+    key: '_mumuki_bibliotheca_session', httponly: false
+>>>>>>> Stashed changes
