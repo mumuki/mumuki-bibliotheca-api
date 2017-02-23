029338180d056728c221ce0e9c257d54bebae285
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-02-23 12:25:26 -0300

    Updating upsert organization events to nuntius 4

--- lib/events.rb
+++ lib/events.rb
@@ -2,4 +2,11 @@ Mumukit::Nuntius::EventConsumer.handle do
   event :UserChanged do |data|
     Bibliotheca::Collection::Users.import_from_json! data[:user]
   end
+
+  [:OrganizationCreated, :OrganizationChanged].each do |it|
+    event it do |payload|
+      organization = payload[:organization]
+      Bibliotheca::Collection::Organizations.upsert! organization if organization[:name] == 'base'
+    end
+  end
 end
