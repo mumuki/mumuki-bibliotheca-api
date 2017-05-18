63558efb1f0c9ddaabc9fbff9070a40a6538ec28
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-05-18 09:10:17 -0300

    Adding spec

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -10,6 +10,7 @@ describe Bibliotheca::Exercise do
     tag_list: %w(baz bar),
     layout: 'input_bottom',
     id: 2}.deep_stringify_keys }
+
   let(:single_json) { {
     type: 'problem',
     name: 'Single',
@@ -31,6 +32,17 @@ describe Bibliotheca::Exercise do
     layout: 'input_bottom',
     id: 2}.deep_stringify_keys }
 
+
+  let(:single_json_text_in_guide) { {
+    type: 'problem',
+    name: 'Single',
+    editor: 'single_choice',
+    choices: [{value: 'foo', checked: true}, {value: 'bar', checked: false}, {value: 'baz', checked: false}],
+    tag_list: %w(baz bar),
+    layout: 'input_bottom',
+    id: 2}.deep_stringify_keys }
+
+
   describe 'process multiple choices' do
     it { expect(Bibliotheca::Exercise.new(multiple_json).type).to eq 'problem' }
     it { expect(Bibliotheca::Exercise.new(multiple_json).name).to eq 'Multiple' }
@@ -38,20 +50,31 @@ describe Bibliotheca::Exercise do
     it { expect(Bibliotheca::Exercise.new(multiple_json).choices).to eq multiple_json['choices'] }
     it { expect(Bibliotheca::Exercise.new(multiple_json).test).to eq "---\nequal: '0:2'\n" }
   end
-  describe 'process single choices' do
+
+  describe 'process single choices with non-text language' do
     it { expect(Bibliotheca::Exercise.new(single_json).type).to eq 'problem' }
     it { expect(Bibliotheca::Exercise.new(single_json).name).to eq 'Single' }
     it { expect(Bibliotheca::Exercise.new(single_json).editor).to eq 'single_choice' }
     it { expect(Bibliotheca::Exercise.new(single_json).choices).to eq single_json['choices'] }
     it { expect(Bibliotheca::Exercise.new(single_json).test).to eq "foo" }
   end
-  describe 'process single choices' do
+
+  describe 'process single choices with text language' do
     it { expect(Bibliotheca::Exercise.new(single_json_text).type).to eq 'problem' }
     it { expect(Bibliotheca::Exercise.new(single_json_text).name).to eq 'Single' }
     it { expect(Bibliotheca::Exercise.new(single_json_text).editor).to eq 'single_choice' }
     it { expect(Bibliotheca::Exercise.new(single_json_text).choices).to eq single_json_text['choices'] }
     it { expect(Bibliotheca::Exercise.new(single_json_text).test).to eq "---\nequal: foo\n" }
   end
+
+  describe 'process single choices with text language in guide' do
+    it { expect(Bibliotheca::Exercise.new(single_json_text_in_guide).type).to eq 'problem' }
+    it { expect(Bibliotheca::Exercise.new(single_json_text_in_guide).name).to eq 'Single' }
+    it { expect(Bibliotheca::Exercise.new(single_json_text_in_guide).editor).to eq 'single_choice' }
+    it { expect(Bibliotheca::Exercise.new(single_json_text_in_guide).choices).to eq single_json_text_in_guide['choices'] }
+    it { expect(Bibliotheca::Exercise.new(single_json_text_in_guide).test).to eq "---\nequal: foo\n" }
+  end
+
 
   describe 'markdownified' do
     context 'description' do
