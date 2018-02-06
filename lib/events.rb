8d83c05732e572191083e15e7b4edd6d2bf59d1a
Federico Scarpa <fedescarpa@gmail.com>
2018-02-06 17:02:49 -0300

    removed some fields from events consumers

--- lib/events.rb
+++ lib/events.rb
@@ -1,11 +1,11 @@
 Mumukit::Nuntius::EventConsumer.handle do
   event :UserChanged do |data|
-    Bibliotheca::Collection::Users.import_from_json! data[:user]
+    Bibliotheca::Collection::Users.import_from_json! data[:user].except(:created_at, :updated_at)
   end
 
   [:OrganizationCreated, :OrganizationChanged].each do |it|
     event it do |payload|
-      Bibliotheca::Collection::Organizations.import_from_json! payload[:organization]
+      Bibliotheca::Collection::Organizations.import_from_json! payload[:organization].except(:created_at, :updated_at)
     end
   end
 end
