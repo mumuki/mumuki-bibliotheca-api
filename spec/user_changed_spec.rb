936837d8be64175cdaf8966215eb15a470647e20
Franco Bulgarelli <franco@mumuki.org>
2018-02-07 11:23:14 -0300

    Not updating permissions if no changes

--- spec/user_changed_spec.rb
+++ spec/user_changed_spec.rb
@@ -3,13 +3,22 @@ require 'spec_helper'
 describe Bibliotheca::Collection::Users do
 
   let(:user) { {uid: uid, permissions: {writer: 'foo/bar'}} }
+  let(:uid) { 'bot@mumuki.org' }
 
-  describe 'should update user permissions' do
-    let(:uid) { 'bot@mumuki.org' }
+  context 'when there are permissions changes' do
     let(:permissions) { Bibliotheca::Collection::Users.find_by_uid!(uid).permissions }
 
     before { Bibliotheca::Collection::Users.import_from_json! user }
 
+    it { expect(permissions.has_permission? :writer, 'foo/bar').to eq true }
+    it { expect(permissions.has_permission? :editor, 'foo/bar').to eq false }
+  end
+
+  context 'when there are no permissions changes' do
+    before { Bibliotheca::Collection::Users.import_from_json! uid: uid, permissions: {writer: 'foo/bar'} }
+    before { Bibliotheca::Collection::Users.import_from_json! uid: uid, permissions: nil }
+
+    let(:permissions) { Bibliotheca::Collection::Users.find_by_uid!(uid).permissions }
     it { expect(permissions.has_permission? :writer, 'foo/bar').to eq true }
     it { expect(permissions.has_permission? :editor, 'foo/bar').to eq false }
   end
