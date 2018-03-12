b5b7ff1238909458e7d9fb58e32ea8e57c3ac9d5
Lucho Cannavo <luchotc@hotmail.com.ar>
2018-03-12 12:55:04 -0300

    adding input_kids layout

--- lib/bibliotheca/exercise.rb
+++ lib/bibliotheca/exercise.rb
@@ -57,7 +57,7 @@ module Bibliotheca
 
     def errors
       [
-        ("Invalid layout #{layout}" unless [nil, 'input_right', 'input_bottom'].include? layout),
+        ("Invalid layout #{layout}" unless [nil, 'input_right', 'input_bottom', 'input_kids'].include? layout),
         ("Invalid editor #{editor}" unless [nil, 'code', 'multiple_choice', 'single_choice', 'hidden', 'text', 'upload'].include? editor),
         ('Name must be present' unless name.present?),
         ('Name must not contain a / character' if name.include? '/'),
