efa24a4ea86320ed2d9fa3073d8b02ed69c1f1fb
Franco Bulgarelli <franco@mumuki.org>
2018-03-29 11:22:58 -0300

    Adding missing helper

--- lib/bibliotheca/organization.rb
+++ lib/bibliotheca/organization.rb
@@ -1,5 +1,7 @@
 module Bibliotheca
   class Organization < Bibliotheca::SchemaDocument
+    include Mumukit::Platform::Organization::Helpers
+
     def schema
       Bibliotheca::Schema::Organization
     end
