99c5e14f95af16158d16d6b33e30d5d6b13429d0
Rodrigo Alfonso <rodri042@gmail.com>
2019-01-07 19:17:16 -0300

    Revert "Now the /books endpoint works exactly like before, it only returns organizations that have the requested book_id"
    This reverts commit b1dc5f9dc3425768748436758419964497622778.

--- lib/mumuki/bibliotheca/sinatra/books.rb
+++ lib/mumuki/bibliotheca/sinatra/books.rb
@@ -22,7 +22,7 @@ class Mumuki::Bibliotheca::App < Sinatra::Application
   end
 
   get '/books/:organization/:repository/organizations' do
-    organizations_for book, Organization.where(book_id: book.id)
+    organizations_for book
   end
 
   post '/books' do
