5872f53b50f7388e259d8d799fde4fa19a394b67
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-04-02 20:23:33 -0300

    Updating dummy schema with verified_names migration

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200127142401) do
+ActiveRecord::Schema.define(version: 20200213175736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -364,6 +364,8 @@ ActiveRecord::Schema.define(version: 20200127142401) do
     t.datetime "last_reminded_date"
     t.date "birthdate"
     t.integer "gender"
+    t.string "verified_first_name"
+    t.string "verified_last_name"
     t.index ["last_organization_id"], name: "index_users_on_last_organization_id"
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
