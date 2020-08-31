3b2101d6220d6e3bb7eb0577b0a16f64984b8b5b
Lucho Cannavo <luchotc@hotmail.com.ar>
2020-08-31 04:24:23 -0300

    Running domain migrations

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200731081757) do
+ActiveRecord::Schema.define(version: 20200804191643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -309,6 +309,7 @@ ActiveRecord::Schema.define(version: 20200731081757) do
     t.text "theme", default: "{}", null: false
     t.text "profile", default: "{}", null: false
     t.integer "progressive_display_lookahead"
+    t.boolean "incognito_mode_enabled"
     t.index ["book_id"], name: "index_organizations_on_book_id"
     t.index ["name"], name: "index_organizations_on_name", unique: true
   end
@@ -385,6 +386,7 @@ ActiveRecord::Schema.define(version: 20200731081757) do
     t.string "verified_last_name"
     t.bigint "avatar_id"
     t.datetime "disabled_at"
+    t.boolean "trusted_for_forum"
     t.index ["disabled_at"], name: "index_users_on_disabled_at"
     t.index ["last_organization_id"], name: "index_users_on_last_organization_id"
     t.index ["uid"], name: "index_users_on_uid", unique: true
